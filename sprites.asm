bf7371f2edba622c16ca549055a6807580d73575
Blake Burgess <trinitek@live.com>
2015-10-23 04:55:22 -0400

    Add some border sprites, border drawing routine

--- sprites.asm
+++ sprites.asm
@@ -1,23 +1,35 @@
 
 sprites:
-    .ball ballSprite c.ALPHA, c.BLUE, c.LIGHTBLUE, c.DARKBLUE
+    .ball ballSprite\
+        c.ALPHA,\
+        c.BLUE,\
+        c.LIGHTBLUE,\
+        c.DARKBLUE
+    .wall hvWallSprite\
+        c.WHITE20,\
+        c.WHITE40,\
+        c.WHITE60,\
+        c.WHITE80,\
+        c.WHITE,\
+        c.WHITE80,\
+        c.WHITE60,\
+        c.WHITE40,\
+        c.WHITE20
 
-; void drawBallSprite(word x, word y)
-; Assumes DS is set to the data segment and ES to the secondary video buffer
-drawBallSprite:
+; void drawSprite(word x, word y, word height, word width, word <si> spritePtr)
+; Assumes DS is set to the data segment and ES to any video buffer
+drawSprite:
     pusha
-    
-    mov si, sprites.ball
     
     push ax
     call coordToPtr
     mov di, ax
     pop ax
     
-    mov cx, ballSprite.height
     .nextY:
         push cx
-        mov cx, ballSprite.width
+        push dx
+        mov cx, dx
         .nextX:
             cmp ax, 320
             ja @f
@@ -34,9 +46,11 @@ drawBallSprite:
             inc di
             inc ax
             loop .nextX
+        pop dx
         pop cx
         
-        add di, 320 - ballSprite.width
+        add di, 320
+        sub di, dx
         dec bx
         loop .nextY
     
@@ -46,51 +60,45 @@ drawBallSprite:
     ret
     
 ; void drawBorder(void)
+; Expects ES to point to buffer B
 drawBorder:
     pusha
     
-    macro d_border axis*, lineType* {
-        mov dl, c.WHITE20
-        call lineType
-        inc axis
-        mov dl, c.WHITE40
-        call lineType
-        inc axis
-        mov dl, c.WHITE60
-        call lineType
-        inc axis
-        mov dl, c.WHITE80
-        call lineType
-        inc axis
-        mov dl, c.WHITE
-        call lineType
-        inc axis
-        mov dl, c.WHITE80
-        call lineType
-        inc axis
-        mov dl, c.WHITE60
-        call lineType
-        inc axis
-        mov dl, c.WHITE40
-        call lineType
-        inc axis
-        mov dl, c.WHITE20
-        call lineType
-    }
-    
     mov ax, field.xMin - 9
-    mov bx, field.yMax
-    mov cx, field.yMax - field.yMin + 1
-    d_border ax, vertLine
+    mov bx, field.yMin
+    mov dx, vertWallSprite.width
+    mov si, sprites.wall
+    mov cx, field.yMax - field.yMin
+    .vert1:
+        push cx
+        mov cx, vertWallSprite.height
+        call drawSprite
+        pop cx
+        inc bx
+        loop .vert1
     
     mov ax, field.xMax
-    mov bx, field.yMax
-    d_border ax, vertLine
+    mov bx, field.yMin
+    mov cx, field.yMax - field.yMin
+    .vert2:
+        push cx
+        mov cx, vertWallSprite.height
+        call drawSprite
+        pop cx
+        inc bx
+        loop .vert2
     
-    mov ax, field.xMin - 1
-    mov bx, field.yMax
-    mov cx, field.xMax - field.xMin + 2
-    d_border bx, horizLine
+    mov ax, field.xMin
+    mov bx, field.yMax + horizWallSprite.height - 1
+    mov dx, horizWallSprite.width
+    mov cx, field.xMax - field.xMin
+    .horiz:
+        push cx
+        mov cx, horizWallSprite.height
+        call drawSprite
+        pop cx
+        inc ax
+        loop .horiz
     
     popa
     ret
